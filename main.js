3cfcdac84513cc96e414f0ece8b2e965ec0d2aca
wellRich <kitty_gyk@163.com>
2018-05-06 09:56:20 +0800

    增添数据表树、搜索功能

--- main.js
+++ main.js
@@ -62,27 +62,134 @@ require.config({
 	}
 });
 
-require(['avalon', "zTree"],function(avalon){
+require(['avalon',  "zTree"],function(avalon){
     avalon.log("引入avalon");
 
-    var model = avalon.define({
+    var sql_parser_vm = avalon.define({
         $id: "root",
         name: "tangolivesky",
-        fullScreen: function () {
-            var de = document.documentElement;
+        zTreeInit: $.fn.zTree.init,
+        data: {
+        	zTreeObj: {},
+        	searchValue: '',
+        	zTreeDom: "tables"
+        },
+        rpt: {
 
-            if (de.requestFullscreen) {
-                de.requestFullscreen();
-            } else if (de.mozRequestFullScreen) {
-                de.mozRequestFullScreen();
-            } else if (de.webkitRequestFullScreen) {
-                de.webkitRequestFullScreen();
-            }
-            else {
-                App.alert({message: "该浏览器不支持全屏！", type: "danger"});
-            }
+        },
+        findInTree: function(){
 
+            // 重写了zTreeSearch中的查询方法
+            // 清除高亮
+			var value = sql_parser_vm.data.searchValue,
+				zTreeObj = sql_parser_vm.data.zTreeObj,
+				allNodes = zTreeObj.transformToArray(zTreeObj.getNodes());
+			console.log("value -----> ", value);
+			for (var i in allNodes) {
+				allNodes[i].highlight = false;
+				zTreeObj.updateNode(allNodes[i]);
+			}
+			console.log("allNodes -----> ", allNodes);
+			if (value == '') return; // input '' wipe highlight
+
+
+			// 查询节点
+			var nodes = zTreeObj.getNodesByParamFuzzy('name', value);
+
+			console.log("nodes -----> ", nodes);
+
+			// 高亮显示匹配的节点，并展开
+			for (var i in nodes) {
+				nodes[i].highlight = true;
+				zTreeObj.updateNode(nodes[i]);
+				zTreeObj.expandNode(nodes[i].getParentNode(), true);
+
+				//定位到匹配的第一个节点
+				if (i == 0) {
+					zTreeObj.selectNode(nodes[0]);
+				}
+			}
+        },
+        draw: function(event) {
+        	console.log("draw---> ", event);
         }
+
     });
     avalon.scan();
+
+    //创建zTree树
+    buildTree();
+	function buildTree() {
+		var data = testData, setting = {
+            check: {
+                enable: true,
+                chkStyle: "checkbox",
+                chkboxType: { "Y": "p", "N": "s" }
+            }
+        }, _search = $('#searchZoning');
+
+        _search.keyup(function(e) {
+            if(e.keyCode === 13){
+                sql_parser_vm.findInTree();
+            }
+        });
+        sql_parser_vm.data.zTreeObj = sql_parser_vm.zTreeInit($("#" + sql_parser_vm.data.zTreeDom), setting, data);
+	}
+
+
 });
+
+//ztree的测试数据
+var testData = [{
+	id: 1000,
+	label: '用户',
+	name: 'user',
+	flag: 'table',
+	children: [{
+		name: 'user_name',
+		type: 'String',
+		label: '用户名'
+	}]
+}, {
+	id: 2000,
+	label: '机构',
+	name: 'dept',
+	flag: 'table',
+	children: [{
+		label: 'id',
+		name: 'id',
+		flag: 'column',
+		type: 'Long'
+	}, {
+		label: '机构简称',
+		name: 'simpleName',
+		flag: 'column',
+		type: 'String'
+	}, {
+		flag: 'column',
+		label: '机构负责人',
+		name: 'leader',
+		type: 'Long'
+	}]
+}, {
+	id: 3000,
+	label: '客户',
+	name: 'client',
+	flag: 'table',
+	children: [{
+		label: 'id',
+		name: 'id',
+		flag: 'column',
+		type: 'Long'
+	}, {
+		label: '名称',
+		name: 'name',
+		flag: 'column',
+		type: 'String'
+	}, {
+		flag: 'column',
+		label: '地址',
+		name: 'adress',
+		type: 'String'
+	}]
+}];
